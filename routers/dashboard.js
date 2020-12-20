b82c1b98672a1c4001383513da8da4a80473c884
Sarvesh Kadam <kadamsarvesh1905@gmail.com>
2020-12-20 17:54:02 +0530

    authentication

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -1,8 +1,9 @@
 const express = require('express')
 
 const router = express.Router()
+const user = require('./user')
 
-router.get('/',(req,res)=>{
+router.get('/',user.isAuthenticatedUser,(req,res)=>{
     res.render('dashboard')
 })
 
